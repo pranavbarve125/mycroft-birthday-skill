25e9cddc0727b1059d9b9e596dca5189c6ca337d
pranavbarve125 <pranavbarve98@gmail.com>
2023-06-22 13:02:22 +1000

    New Update

--- __init__.py
+++ __init__.py
@@ -1,14 +1,20 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.skills.api import SkillApi
 import time
 
 class Birthday(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.txt = ""
         
     @intent_file_handler('birthday.intent')
     def handle_birthday(self, message):
-        self.dialog('Acknowledged')
+        duration = message.data.get('duration')
+        
+        with open('/home/pranav125/Desktop/mycroft-core/skills/birthday-skill/morning.txt', 'r') as f:
+            self.txt = str(f.read())
+        
+        self.speak(self.txt)
 
 def create_skill():
     return Birthday()
-
