8ab61289f64411dd5a6804f6b562a0477ceafef0
pranavbarve125 <pranavbarve98@gmail.com>
2023-06-28 23:04:17 +1000

    Normal telegram can be used, i guess

--- __init__.py
+++ __init__.py
@@ -1,22 +1,26 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.skills.api import SkillApi
 import re
 
-from .google_calendar_api import get_calendar_events
+from .google_calendar_api import GoogleCalendar
 
 class Birthday(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.txt = ""
+        self.gc = GoogleCalendar()
+        self.tele = SkillApi.get('telegram.luke5sky')
         
     @intent_file_handler('duration.intent')
-    def handle_birthday(self, message):
+    def handle_birthdays_duration(self, message): # week, month, tommorrow, today
         duration_entity = message.data.get('duration')
-        date_entity = date_entity = message.data.get('date')
-        
-        self.txt = get_calendar_events()
-        
+        self.txt = self.gc.get_calendar_events_for_a_duration(duration_entity)
         self.speak(self.txt)
+        msg = 'works'
+        self.speak(self.tele.sendTelegramMessage(msg))
 
+    def handle_birthdays_date(self, message):
+        date_entity = date_entity = message.data.get('date')
 
 def create_skill():
     return Birthday()
--- google_calendar_api.py
+++ google_calendar_api.py
@@ -4,43 +4,56 @@ from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 import re
 
-def format_date(date_string):
-    # Convert the date string to a datetime object
-    date = datetime.datetime.strptime(date_string, "%Y-%m-%d")
 
-    # Extract the day with suffix (e.g., 23rd, 1st)
-    day = date.strftime("%d")
-    if day.endswith(('11', '12', '13')):
-        day += "th"
-    elif day.endswith('1'):
-        day += "st"
-    elif day.endswith('2'):
-        day += "nd"
-    elif day.endswith('3'):
-        day += "rd"
-    else:
-        day += "th"
-
-    formatted_date = date.strftime(f"{day} %B")
-    return formatted_date
-
-
-def get_calendar_events(end_date=None):
+class GoogleCalendar:
+    def __init__(self):
         # Load the credentials from the JSON file you downloaded
         credentials = Credentials.from_authorized_user_file('/home/pranav125/Desktop/mycroft-core/skills/birthday-skill/credentials/google_credentials.json')
 
         # Build the service using the credentials
-        service = build('calendar', 'v3', credentials=credentials)
+        self.service = build('calendar', 'v3', credentials=credentials)
 
+    def format_date(self, date_string):
+        # Convert the date string to a datetime object
+        date = datetime.datetime.strptime(date_string, "%Y-%m-%d")
+
+        # Extract the day with suffix (e.g., 23rd, 1st)
+        day = date.strftime("%d")
+        if day.endswith(('11', '12', '13')):
+            day += "th"
+        elif day.endswith('1'):
+            day += "st"
+        elif day.endswith('2'):
+            day += "nd"
+        elif day.endswith('3'):
+            day += "rd"
+        else:
+            day += "th"
+
+        formatted_date = date.strftime(f"{day} %B")
+        return formatted_date
+
+
+    def get_calendar_events_for_a_duration(self, duration_entity):
         # Get the current date and time
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        current_date = datetime.datetime.utcnow()#.isoformat() + 'Z'  # 'Z' indicates UTC time
 
         # Calculate the date for one week from now
-        one_week_from_now = (datetime.datetime.utcnow() + datetime.timedelta(days=365)).isoformat() + 'Z'
 
-        end_date = one_week_from_now
+        if duration_entity == "week":
+            end_date = current_date + datetime.timedelta(weeks=1)
+        elif duration_entity == "month":
+            end_date = current_date + datetime.timedelta(days=30)
+        elif duration_entity == "today":
+            end_date = current_date
+        elif duration_entity == "tomorrow":
+            end_date = current_date + datetime.timedelta(days=1)
+
+        formatted_current_date = current_date.isoformat() + 'Z'
+        formatted_end_date = end_date.isoformat() + 'Z'
+
         # Call the Calendar API to retrieve the events
-        events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=end_date, singleEvents=True,
+        events_result = self.service.events().list(calendarId='primary', timeMin=formatted_current_date, timeMax=formatted_end_date, singleEvents=True,
                                             orderBy='startTime').execute()
         events = events_result.get('items', [])
         print(events_result)
@@ -53,12 +66,12 @@ def get_calendar_events(end_date=None):
             start = event['start'].get('dateTime', event['start'].get('date'))
             event_summary = event['summary']
             if event_summary.startswith('Birthday -'):
-                formatted_date = format_date(start)
+                formatted_date = self.format_date(start)
                 name = re.sub(r'^Birthday - ', '', event_summary)
                 events_list.append((formatted_date, name))
         
         # prepare a string
-        return_string = "There are {} birthdays for the specified duration.\n".format(len(events_list))
+        return_string = "There are {} birthday(s) for the specified duration.\n".format(len(events_list))
         for date, name in events_list:
             return_string += f'{name} {date}.\n'
 
