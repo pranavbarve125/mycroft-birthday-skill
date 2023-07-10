bbf07145dc1744e646b73eac11e81670df34b7a5
pranavbarve125 <pranavbarve98@gmail.com>
2023-07-10 15:11:24 +1000

    Solved

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
+from apscheduler.schedulers.background import BackgroundScheduler
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.skills.api import SkillApi
+import time
+
 import re
 
 from .google_calendar_api import GoogleCalendar
@@ -8,26 +11,16 @@ class Birthday(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.txt = ""
-        self.events = ""
         self.gc = GoogleCalendar()
         self.tele = SkillApi.get('telegram.luke5sky')
 
-        # self.scheduler = BackgroundScheduler()
-        # self.scheduler.add_job(self.read_daily_birthdays, 'cron', hour='9,15,21', minute='0,30')
+        self.scheduler = BackgroundScheduler()
+        self.scheduler.add_job(self.read_daily_birthdays, 'cron', hour='9,15,21', minute='0,30')
 
     def initialize(self):
         self.scheduler.start()
 
-    def read_morning_script(self):
-        self.txt = self.gc.get_calendar_events_for_a_duration("today")
-        
-        if self.txt == "Please specify correct duration.":
-            return
-        
-        self.speak(self.txt)
-        self.tele.sendTelegramMessage(self.txt)
-
-    def read_afternoon_evening_script(self):
+    def read_daily_birthdays(self):
         self.txt = self.gc.get_calendar_events_for_a_duration("today")
         
         if self.txt == "Please specify correct duration.":
@@ -39,19 +32,11 @@ class Birthday(MycroftSkill):
     @intent_file_handler('duration.intent')
     def handle_birthdays_duration(self, message): # week, month, tommorrow, today
         duration_entity = message.data.get('duration')
-        self.events = self.gc.get_calendar_events_for_a_duration(duration_entity)
-
-        # prepare a string
-        self.txt = "There are {} birthday(s) for the specified duration.\n".format(len(self.events))
-        for date, name in self.events:
-            self.txt += f'{name} {date}.\n'
-
+        self.txt = self.gc.get_calendar_events_for_a_duration(duration_entity)
         self.speak(self.txt)
-        msg = 'works'
-        self.speak(self.tele.sendTelegramMessage(msg))
-
-    def handle_birthdays_date(self, message):
-        date_entity = date_entity = message.data.get('date')
+        # self.tele.sendTelegramMessage(self.txt)
+        # self.speak(self.tele.sendTelegramMessage(msg))
 
 def create_skill():
     return Birthday()
+    
